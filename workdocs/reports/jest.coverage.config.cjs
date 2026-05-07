7e88970db6c2a1ef2223e8eaba53b2eda60c5efb
tvenceslau <venceslau.tiago@gmail.com>
2026-05-07 21:07:50 +0100

    patch - update-no-ci - after release preparation

---
+++ workdocs/reports/jest.coverage.config.cjs
@@ -0,0 +1,59 @@
+// import { Config } from "@jest/types";
+const base = require("../../jest.config.cjs");
+
+module.exports = {
+  ...base,
+  collectCoverageFrom: [
+    "src/index.ts",
+    "src/output/**/*.ts",
+    "src/writers/**/*.ts",
+    "src/utils/constants.ts",
+    "src/utils/timeout.ts",
+  ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "src/cli/",
+    "src/input/",
+    "src/utils/fs.ts",
+    "src/utils/http.ts",
+    "src/utils/tests.ts",
+    "src/utils/utils.ts",
+  ],
+  // ...conf,
+  collectCoverage: true,
+  coverageDirectory: "./workdocs/reports/coverage",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "./workdocs/reports/junit",
+        outputName: "junit-report.xml",
+      },
+    ],
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./workdocs/reports/html",
+        filename: "test-report.html",
+        openReport: true,
+        expand: true,
+        pageTitle: "@decaf-ts/utils",
+        stripSkippedTest: true,
+        darkTheme: true,
+        enableMergeData: true,
+        dataMergeLevel: 2,
+      },
+    ],
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 26,
+      functions: 45,
+      lines: 39,
+      statements: 40,
+    },
+  },
+};
+
+// export default config;
